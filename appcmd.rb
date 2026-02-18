e8b78a1f7c88c9e94cc3301c0486e9e6c05d12b0
970uraj <utkarsh.raj@sonova.com>
2026-02-18 19:44:22 +0100

    Update appcmd to v1.0.2

--- appcmd.rb
+++ appcmd.rb
@@ -1,8 +1,8 @@
 class Appcmd < Formula
   desc "Right-Command based app switcher for macOS"
   homepage "https://github.com/clearnote01/appcmd"
-  url "https://github.com/clearnote01/appcmd/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "586d56676b9bfe092d44a4bc02680f6787255e8a875f1612706e1a8f1baaeddd"
+  url "https://github.com/clearnote01/appcmd/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "0ae54bbca18db33ce2b5aac339fdb508823b3d7da14e0b0953fab63d7342b172"
   license "MIT"
 
 
