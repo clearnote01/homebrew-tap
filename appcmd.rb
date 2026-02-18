af6cce4985ad0f252c942456cb3175331e40ceaa
970uraj <utkarsh.raj@sonova.com>
2026-02-18 20:03:19 +0100

    Update appcmd to v1.0.7 with inreplace versioning

--- appcmd.rb
+++ appcmd.rb
@@ -1,13 +1,13 @@
 class Appcmd < Formula
   desc "Right-Command based app switcher for macOS"
   homepage "https://github.com/clearnote01/appcmd"
-  url "https://github.com/clearnote01/appcmd/archive/refs/tags/v1.0.6.tar.gz"
-  sha256 "05118c3d9b29fb1d39a09b7715d95a1ba68f25cc2209db73327e853153a0fd04"
+  url "https://github.com/clearnote01/appcmd/archive/refs/tags/v1.0.7.tar.gz"
+  sha256 "5fc2985d1aae6678e73e6230469c13e6a5a256bfc18b2ae984f7cb22f8a529a7"
   license "MIT"
 
   def install
-    # Dynamically inject the Homebrew version into the Info.plist before building
-    system "/usr/libexec/PlistBuddy", "-c", "Set :CFBundleShortVersionString #{version}", "Sources/Info.plist"
+    # Dynamically inject the version into the source code before building
+    inreplace "Sources/Version.swift", 'static let current = "dev"', "static let current = \"#{version}\""
     
     system "swift", "build", "--disable-sandbox", "-c", "release"
     bin.install ".build/release/appcmd"
@@ -23,7 +23,7 @@ class Appcmd < Formula
     <<~EOS
       After installation/upgrade, you must grant Accessibility permissions:
       1. Open System Settings > Privacy & Security > Accessibility.
-      2. If "appcmd" is already there, remove it with the (-) button and add it again.
+      2. If \"appcmd\" is already there, remove it with the (-) button and add it again.
       3. Add: #{opt_bin}/appcmd
     EOS
   end
